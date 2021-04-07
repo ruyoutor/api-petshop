8a22e0f8628f8f0c00fc8f1be16492d7ef657c7a
Ruy dos Santos Matias Outor <ruyoutor@MacBook-Air-de-Ruy.local>
2021-04-06 23:45:38 -0300

    complemento

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -5,6 +5,7 @@ const Tabela = require('./TabelaFornecedor')
 roteador.use('/', async (req, res) => {
     const resultado = await Tabela.listar();
     res.status(200).json(resultado);
+    
 })
 
 module.exports = roteador;
